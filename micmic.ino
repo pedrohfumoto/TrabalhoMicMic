e8691eab95bee970b4567859a83d45d32c8699ac
MatheusPoli <127156466+MatheusPoli@users.noreply.github.com>
2023-03-14 09:59:08 -0300

    Update micmic.ino

--- micmic.ino
+++ micmic.ino
@@ -1,11 +1,13 @@
 #include <WiFi.h>
 
-#define led 2
+#define led_red 12
+#define led_blue 13
+#define led_green 14
 
 String ClientRequest;
 
-IPAddress staticIP(192,168,0,48);
-IPAddress gateway(192,168,0,1);
+IPAddress staticIP(192,168,43,124);
+IPAddress gateway(192,168,43,1);
 IPAddress subnet(255,255,255,0);
 
 WiFiServer server(80);
@@ -33,14 +35,20 @@ void setup() {
   
   ClientRequest = "";
 
-  pinMode(led, OUTPUT);
+  pinMode(led_red, OUTPUT);
+  pinMode(led_blue, OUTPUT);
+  pinMode(led_green, OUTPUT);
 
   Serial.begin(115200);
   delay(10);
 
   Serial.println("START");
 
-  WiFi.begin("eduroam", "758912");
+  WiFi.begin("iPhone (3)", "micmic123");
+  WiFi.softAP("iPhone (3)", "micmic123");
+  Serial.print("Access point running. IP address: ");
+  Serial.print(WiFi.softAPIP());
+  Serial.println("");
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -50,7 +58,7 @@ void setup() {
 
   Serial.println("Connected");
 
-  WiFi.config(staticIP, gateway, subnet);
+  //WiFi.config(staticIP, gateway, subnet);
 
   Serial.println("Your IP is");
   Serial.println((WiFi.localIP()));
@@ -74,29 +82,57 @@ void loop() {
   ClientRequest.remove(0, 5);
   ClientRequest.remove(ClientRequest.length()-9,9);
 
-  if (ClientRequest == "acender")
-    digitalWrite(led, HIGH);
-  if (ClientRequest == "apagar")
-    digitalWrite(led, LOW);
-  if (ClientRequest == "piscar")
+  if (ClientRequest == "Amarelo")
   {
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
-  }
+    analogWrite(led_red, 255);
+    analogWrite(led_green, 255);
+    analogWrite(led_blue, 0);}
+    
+  if (ClientRequest == "apagar")
+  {  
+    analogWrite(led_red, 0);
+    analogWrite(led_green, 0);
+    analogWrite(led_blue, 0);}
+ // if (ClientRequest == "piscar")
+  //{
+   // digitalWrite(led, HIGH);
+    //delay(500);
+    //digitalWrite(led, LOW);
+    //delay(500);
+    //digitalWrite(led, HIGH);
+    //delay(500);
+    //digitalWrite(led, LOW);
+    //delay(500);
+    //digitalWrite(led, HIGH);
+    //delay(500);
+    //digitalWrite(led, LOW);
+    //delay(500);
+    //digitalWrite(led, HIGH);
+    //delay(500);
+    //digitalWrite(led, LOW);
+    //delay(500);
+ // }
+
+  if (ClientRequest == "Verde")
+    {  
+    analogWrite(led_red, 0);
+    analogWrite(led_green, 255);
+    analogWrite(led_blue, 0);
+    }
+
+  if (ClientRequest == "Azul")
+   {  
+    analogWrite(led_red, 0);
+    analogWrite(led_green, 0);
+    analogWrite(led_blue, 255);
+    }
+
+  if (ClientRequest == "Vermelho")
+{  
+    analogWrite(led_red, 255);
+    analogWrite(led_green, 0);
+    analogWrite(led_blue, 0);}    
+  
 
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
